cb59367ac27598fd17b36f00ba52909aa4589378
GarveyZhou <gzhou2@stuy.edu>
2018-02-14 15:30:55 -0500

    started KNighttour

---
+++ 03Nights/KnightBoard.java
@@ -0,0 +1,26 @@
+public class KnightBoard{
+    private int[][] board;
+
+    
+    public KnightBoard(int r, int c){
+	board = new int[r][c];
+	for (int i = 0; i < r, i++){
+	    for(int j = 0; j < c; j++){
+		board[i][j] = 0;
+	    }
+	}
+    }
+
+    public String toString(){
+
+    }
+
+    public boolean solve(int row,int col){
+
+    }
+
+    public int countSolutions(int row,int col){
+
+    }
+    
+}
