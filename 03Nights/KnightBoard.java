83f2e9c7a9a3c9009d7ea5e38b785df17777a28d
GarveyZhou <gzhou2@stuy.edu>
2018-02-15 15:27:12 -0500

    help

--- 03Nights/KnightBoard.java
+++ 03Nights/KnightBoard.java
@@ -1,8 +1,10 @@
 public class KnightBoard{
     private int[][] board;
-
+    private int[][] moves;
     
     public KnightBoard(int r, int c){
+	moves = new int[8][2];
+	moves = {{-2,1} , {-2,-1} , {-1,-2} , {1,2} , {2,-1} , {2,1} , {1,2}, {-1,2} }; 
 	board = new int[r][c];
 	for (int i = 0; i < r, i++){
 	    for(int j = 0; j < c; j++){
@@ -12,13 +14,37 @@ public class KnightBoard{
     }
 
     public String toString(){
-
+	String res = "";
+	for(int i = 0; i <board.length;i++){
+	    for(int j = 0; j <board[0].length;j++){
+		if ( board [i][j] == 0){
+		    res += "_ ";
+		}
+		else{res += board[i][j] + " ";}
+	    }
+	}
     }
-
+    
     public boolean solve(int row,int col){
-
+	if(row >= board.length || col >= board[0].length || row < 0 || col < 0){
+	    throw new IllegalArgumentException();
+	}
+	for(int i = 0; i < board.length; i++){
+	    for(int j =0; j< board.length; j++){
+		if(board[i][j] != 0){
+		    throw new IllegalStateException;
+		}
+	    }
+	}
+	for( int i = 0; i < 8; i++){
+	    
+	}    
     }
 
+    public boolean help(int row,int col, int type){
+	
+    }
+    
     public int countSolutions(int row,int col){
 
     }
