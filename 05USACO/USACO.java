c5248b39e6eb0ab890f85b1dee1f91334f8b3fed
GarveyZhou <gzhou2@stuy.edu>
2018-03-09 15:29:39 -0500

    tested bronze

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -6,31 +6,31 @@ public class USACO{
 	try{
 	    File f = new File(filename);
 	    Scanner s = new Scanner(f);
-	int R = s.nextInt();
-	int C = s.nextInt();
-	int E = s.nextInt();
-	int N = s.nextInt();
-	int[][] land = new int[R][C];
-	for(int i = 0; i <R; i++){
-	    for(int j = 0;j < C ; j++){
-		land[i][j] = s.nextInt();
-	    }
-	}
-	for (int n = 0; n< N; n--){
-	    int r = s.nextInt();
-	    int c = s.nextInt();
-	    int down = s.nextInt();
-	    stomp(land,r - 1 ,c - 1,down);
-	}
-	int totalDepth = 0;
-	for( int i = 0; i< R; i++){
-	    for(int j = 0; j< C;j++){
-		if(land[i][j] < E){
-		    totalDepth += E - land[i][j];
+	    int R = s.nextInt();
+	    int C = s.nextInt();
+	    int E = s.nextInt();
+	    int N = s.nextInt();
+	    int[][] land = new int[R][C];
+	    for(int i = 0; i <R; i++){
+		for(int j = 0;j < C ; j++){
+		    land[i][j] = s.nextInt();
 		}
 	    }
-	}
-	return totalDepth *72*72;
+	    for (int n = 0; n < N; n++){
+		int r = s.nextInt();
+		int c = s.nextInt();
+		int down = s.nextInt();
+		stomp(land,r - 1 ,c - 1,down);
+	    }
+	    int totalDepth = 0;
+	    for( int i = 0; i< R; i++){
+		for(int j = 0; j< C;j++){
+		    if(land[i][j] < E){
+			totalDepth += E - land[i][j];
+		    }
+		}
+	    }
+	    return totalDepth * 72 * 72;
 	
 	} catch(FileNotFoundException e){};
 	return -1;
@@ -38,29 +38,40 @@ public class USACO{
 
     public static void stomp(int[][] data, int row, int col ,int d){
 	int max = 0;
-	for(int i = 0; i < 3;i++){
-	    for(int j = 0; j < 3;j++){
-		if(data[row+i][col+j] > max){
-		    max = data[row+i][j+col];
+	try{
+	    for(int i = 0; i < 3;i++){
+		for(int j = 0; j < 3;j++){
+		    if(data[row+i][col+j] > max){
+			max = data[row+i][j+col];
+		    }
 		}
 	    }
-	}
-	for(int i = 0; i < 3;i++){
-	    for(int j = 0; j < 3;j++){
-		if(data[row+i][col+j] > max - d){
-		    data[row+i][j+col] = max -d;
+	    for(int i = 0; i < 3;i++){
+		for(int j = 0; j < 3;j++){
+		    if(data[row+i][col+j] > max - d){
+			data[row+i][j+col] = max -d;
+		    }
 		}
 	    }
-	}
+	}catch(ArrayIndexOutOfBoundsException e){};
     }
 
     public static int silver(String filename){
 	try{ File f = new File(filename);
 	    Scanner s = new Scanner(f);
+	    int R  = s.nextInt();
+	    int C = s.nextInt();
+	    int T = s.nextInt();
+	    char[][] map = new char[R][C];
+	    
+	    
 
-
-	}catch(Exceptions e){};
+	}catch(Exception e){};
+	return 0;
+    }
+    
     public static void main(String[]args){
+	System.out.println(USACO.bronze("Lakemake1.txt"));
     }
     
 }
