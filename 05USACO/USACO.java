533979c6af0d928d4bd409ca82b221067d84a2ad
unknown <gzhou2@stuy.edu>
2018-03-11 18:15:22 -0400

    added arrays for silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -63,13 +63,44 @@ public class USACO{
 	    int C = s.nextInt();
 	    int T = s.nextInt();
 	    char[][] map = new char[R][C];
-	    
-	    
+	    for(int i = 0; i< R; i++){
+		String row = s.nextLine();
+		for(int j = 0; j< C ; j++){
+		    map[i][j] = row.charAt(j);
+		}
+	    }
+	    int[][] newarr = new int [R][C];
+	    int[][] oldarr = new int[R][C];
 
+	    for(int i = 0; i<R;i++){
+		for(int j = 0; j <C: j++){
+		    if (map[i][j] == '*'){
+			oldarr[i][j] = -1;
+		    }
+		    else{oldarr[i][j] == 0;
+		    }
+		}
+	    }
+
+	    int R1 = s.nextInt() - 1;
+	    int C1 = s.nextInt() - 1;
+	    int R2 = s.nextInt() - 1;
+	    int C2 = s.nextInt() -1;
+	    oldarr[R1][C1]= 1;
+	    if((R2-R1+C2-C1)%2 != T%2){
+		return 0;}
+	    
 	}catch(Exception e){};
 	return 0;
     }
-    
+
+
+    public static void sumNeightbors(int r, int c,int arr){
+	int[][] sides = {{0,1},{0,-1},{1,0},{-1,0}};
+	in sum = 0;
+	for (int i = 0 ; i< 4; i++){
+	    try{
+	    
     public static void main(String[]args){
 	System.out.println(USACO.bronze("Lakemake1.txt"));
     }
