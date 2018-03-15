cd6a9d11ec98e34bcb01e13609bdd9b5bcca8d24
unknown <gzhou2@stuy.edu>
2018-03-14 22:04:36 -0400

    added quickselect

--- 06Quickly!/Quick.java
+++ 06Quickly!/Quick.java
@@ -1,21 +1,20 @@
 import java.util.*;
 public class Quick{
 
-    public int partition(int[] data, int start, int end){
-	int pivot = (int)(Math.random()*data.length);
-	swap(data,pivot,0);
-	for (int i = 1; i < data.length && start < end; i++){
-	    if(data[pivot] >= data[i]){
-		swap(data,i,start);
-		start++;
+    public static int partition(int[] data, int start, int end){
+	int pivot = (int)(Math.random()*(end-start)+start);
+	swap(data,start,pivot);
+	int i = start + 1;
+	while (i <=  end){
+	    if(data[i] <=  data[start]){
+		i++;
 	    }
-	    else{ swap(data,i,end);
+	    else{swap(data,i,end);
 		end--;
-		i--;
 	    }
 	}
-	swap(data,0,start);
-	return start;
+	swap(data,start,end);
+	return end;
 	
     }
 
@@ -23,5 +22,30 @@ public class Quick{
 	int c = ary[a];
 	ary[a] = ary[b];
 	ary[b] = c;
+    }
+
+    public static String toString(int[] data){
+	String s = "[";
+	for(int i =0; i<data.length; i++){
+	    s += data[i] + " ";
+	}
+	s += "]";
+	return s;
+    }
+    public static int quickselect(int[]data,int k){
+	int start = 0;
+	int end = data.length -1;
+	int in = partition(data,start,end);
+	while(in != k){
+	    if (in < k){
+	        start = in + 1;
+	    }
+	    else{end = in - 1;}
+	    in = partition(data,start,end);
+	}
+	return data[in];	
+    }
+    public static void main(String[]args){
+	    
     }
 }
