717ce3224357d36d213fddef6a9d1bb896f7c5ad
GarveyZhou <gzhou2@stuy.edu>
2018-03-20 15:15:13 -0400

    added mergesort

---
+++ 07eMergeing/Merge.java
@@ -0,0 +1,19 @@
+public class Merge{
+
+    public static void mergesort(int[] data){
+	int[] temp = new int[data.length];
+	for(int i = 0; i< data.length; i++){
+	    temp[i] = data[i];
+	}
+	msort(data,temp,0,data.length);
+    }
+
+    private static void msort(int[]data,int[]temp,int lo , int hi){
+	    
+    }
+
+    private static void merge(int[] data,int[]temp,int lo , int hi){
+
+    }
+
+}
