747b285ce8903d3e214066ab332f7167fe5cb455
GarveyZhou <gzhou2@stuy.edu>
2018-03-20 15:24:53 -0400

    change mergesort

--- 07eMergeing/Merge.java
+++ 07eMergeing/Merge.java
@@ -2,14 +2,18 @@ public class Merge{
 
     public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
-	for(int i = 0; i< data.length; i++){
-	    temp[i] = data[i];
-	}
 	msort(data,temp,0,data.length);
     }
 
     private static void msort(int[]data,int[]temp,int lo , int hi){
-	    
+	if (lo <= hi){
+	    for(int i = 0; i< data.length; i++){
+		temp[i] = data[i];
+	    }
+	    int mid = (hi+lo)/2;
+	    msort(temp,data,lo,mid);
+	    msort(temp,data,mid+1,hi);
+	}
     }
 
     private static void merge(int[] data,int[]temp,int lo , int hi){
