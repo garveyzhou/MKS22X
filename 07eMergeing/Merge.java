0fb6ec21d5e9df65b511bdd887c42ba8d80005b1
GarveyZhou <gzhou2@stuy.edu>
2018-03-23 15:27:50 -0400

    worked on merge

--- 07eMergeing/Merge.java
+++ 07eMergeing/Merge.java
@@ -2,19 +2,20 @@ public class Merge{
 
     public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
+
 	msort(data,temp,0,data.length);
     }
 
     private static void msort(int[]data,int[]temp,int lo , int hi){
-	if (lo <= hi){
-	    for(int i = 0; i< data.length; i++){
+	if (lo < hi){
+	    for(int i = lo; i< hi; i++){
 		temp[i] = data[i];
 	    }
 	    int mid = (hi+lo)/2;
 	    msort(temp,data,lo,mid);
 	    msort(temp,data,mid+1,hi);
+	    merge(temp,data,lo,mid,hi);
 	}
-	else{merge(temp,data,lo,hi);}
     }
 
      private static void swap(int[]ary,int a, int b){                  
@@ -23,10 +24,23 @@ public class Merge{
 	ary[b] = c;
     }
 
-    private static void merge(int[] data,int[]temp,int lo , int hi){
-	if(data[lo] > data[hi]){
-	    swap(data,lo,hi);
-	}
+    private static void merge(int[] data,int[]temp,int lo , int mid, int hi){
+	int l=lo;
+	int m = mid + 1;
+	for(int i = 0; i < hi;i++){
+	    /*
+	    if(m <= end){
+		if(data[l] < data[m]){
+		    temp[i] = data[l];
+		    l++;
+		}
+		else{ temp[i] = data[m];
+		    m++;
+		}
+	    }
+	    */
+	    
+	    
     }
 
 }
