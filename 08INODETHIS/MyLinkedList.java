5d279dc85e0b791941c3e01accde0fe8e2075e71
unknown <gzhou2@stuy.edu>
2018-04-09 23:06:54 -0400

    finish functions other than add and remove

--- 08INODETHIS/MyLinkedList.java
+++ 08INODETHIS/MyLinkedList.java
@@ -1,16 +1,14 @@
 public class MyLinkedList{
-    private Node previous;
-    private Node next;
-    private int data;
+    private Node first;
+    private Node last;
+    private int size;
 
     private class Node{
 	private Node prev;
 	private Node next;
 	private int data;
-	public Node(int data, Node next,Node prev){
-	    data = data;
-	    next = next;
-	    prev = prev;
+	public Node(Node a,Node b ,int info){
+	    prev = a;
 	}
 	public int getData(){
 	    return data;
@@ -30,24 +28,68 @@ public class MyLinkedList{
 	}
 	public String toString(){
 	    String s = "";
-	    s += "["+ prev + " , " + data + " , " + next + "]";
+	    s += "[" + data + "]";
 	    return s;
 	}
     }
     public MyLinkedList(){
+	size = 0;
     }
     
     public String toString(){
+	String str = "";
+	Node current = first;
+	while(current != null){
+	    str += current;
+	    if(current.getNext() != Null){
+		str += ",";
+	    }
+	    current = current.getNext();
+	}
+	return str;
     }
-    public int get(n){
+    private Node  getNode(int n){
+	Node current = first;
+	for(int i = 0; i< n; i++){
+	    current = current.getNext();
+	}
+	return current;
+    }
+       
+    public int get(int n){
+	if( n >= size || n < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	return getNode(n),getData();			 
     }
     public void set(int index,int value){
     }
     public int size(){
+	return size;
     }
     public void add(int index,int value){
     }
+    public boolean add(int newData){
+	Node add = newNode(newData);
+	if (length == 0){
+	    first = add;
+	}
+	else{
+	    last.setNext(add);
+	    add.setPrev(last);
+	}
+	last = toAdd;
+	size ++;
+	return true;
+    }
+    public boolean remove(int value){
+    }
     public void remove(int index){
+    }
+    public void clear(){
+	first = null;
+	last = null;
+	size = 0;
     }
 
     
