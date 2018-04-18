f0bc527e637195de9b3896877745569c833b2373
GarveyZhou <gzhou2@stuy.edu>
2018-04-18 15:11:16 -0400

    added DEque

---
+++ 09EvaluateMe/Calculator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Calculator{
+
+    public static double eval(String e){
+	int numTokens = 1;
+	for(int i = 0; i < e.length(); i++){
+	    if(e.substring(i,i+1).equals(" ")){
+		numTokens ++;
+	    }
+	}
+	String[] tokens = new String[numTokens];
+	int index = 0;
+	for(int i = 0; i< e.length(); i++){
+	    String current = "";
+	    if(e.substring(i,i+1).equals(" ")){
+		tokens[index] = current;
+		current = "";
+		index++;
+	    }
+	    else{
+		current += s.charAt(i);
+	    }
+	}
+	
+	Stack<Double> values = new Stack();
+	
+	return 0;
+    }
+
+
+}
