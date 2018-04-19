f8cc8d81eac1134b365a1238ef6be5af04310563
GarveyZhou <gzhou2@stuy.edu>
2018-04-19 15:20:03 -0400

    tested calculator

--- 09EvaluateMe/Calculator.java
+++ 09EvaluateMe/Calculator.java
@@ -2,26 +2,7 @@ import java.util.*;
 public class Calculator{
     @SuppressWarnings("unchecked")
     public static Double eval(String e){
-	int numTokens = 1;
-	for(int i = 0; i < e.length(); i++){
-	    if(e.substring(i,i+1).equals(" ")){
-		numTokens ++;
-	    }
-	}
-	String[] tokens = new String[numTokens];
-	int index = 0;
-	for(int i = 0; i< e.length(); i++){
-	    String current = "";
-	    if(e.substring(i,i+1).equals(" ")){
-		System.out.println(current);
-		tokens[index] = current;
-		current = "";
-		index++;
-	    }
-	    else{
-		current += e.substring(i,i+1);
-	    }
-	}
+	String[] tokens = e.split(" ");
 	
 	Stack<Double> Tokens = new Stack();
 	for(int i = 0; i < tokens.length; i++){
@@ -29,15 +10,23 @@ public class Calculator{
 		Tokens.push(Tokens.pop() + Tokens.pop());
 	    }
 	    else if(tokens[i].equals("-")){
-		Tokens.push(Tokens.pop() - Tokens.pop());
+		Double a = Tokens.pop();
+		Tokens.push( Tokens.pop() - a);
 	    }
 	    else if(tokens[i].equals("*")){
-		Tokens.push(Tokens.pop() * Tokens.pop());
+		Double a = Tokens.pop();
+		Tokens.push(a * Tokens.pop());
 	    }
 	    else if(tokens[i].equals("/")){
-		Tokens.push(Tokens.pop()/Tokens.pop());
+		Double a = Tokens.pop();
+		Tokens.push(Tokens.pop()/a);
+	    }
+	    else if(tokens[i].equals("%")){
+		Double a = Tokens.pop();
+		Tokens.push( Tokens.pop() % a);
 	    }
 	    else{Tokens.push(Double.parseDouble(tokens[i]));
+		
 	    }
 	}
 
@@ -47,6 +36,9 @@ public class Calculator{
     }
 
     public static void main(String[] args){
-	System.out.println(eval("10 2 +"));
+	System.out.println(eval("10.0 2.0 -"));
+	System.out.println(eval("10.0 2.0 - 2 %"));
+	System.out.println(eval("10.0 2.0 - 5 +"));
+	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
     }
 }
