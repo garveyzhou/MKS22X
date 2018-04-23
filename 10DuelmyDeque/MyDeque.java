753b7a6cd366c2121b93c69cf3e2f6d86e6849ca
unknown <gzhou2@stuy.edu>
2018-04-22 22:05:52 -0400

    added Deque,not tested yet

--- 10DuelmyDeque/MyDeque.java
+++ 10DuelmyDeque/MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<T>{
     private T[] data;
     private int size;
@@ -23,28 +24,122 @@ public class MyDeque<T>{
 	return size;
     }
 
-    public void addFirst(E){
-
+    public void addFirst(T ele ){
+	if (ele == null){
+	    throw new NullPointerException();
+	}
+	if(size == data.length){
+	    resize();
+	}
+	if(size == 0){
+	    first = 0;
+	    last = 0;
+	    data[first] = ele;
+	}
+	else if(first == 0){
+	    first = data.length - 1;
+	    data[first] = ele;
+	}
+	else{first --;
+	    data[first] = ele;
+	}
+	size++;
     }
 
-    public void addLast(E){
-
+    public void addLast(T ele){
+	if (ele == null){
+	    throw new NullPointerException();
+	}
+	if(size == data.length){
+	    resize();
+	}
+	if(size == 0){
+	    first = 0;
+	    last = 0;
+	    data[first] = ele;
+	}
+	else if( last  == data.length - 1){
+	    last = 0;
+	    data[last] = ele;
+	}
+	else{last++;
+	    data[last] = ele;
+	}
+	size++;
     }
 
-    public E removeFirst(){
+    public T removeFirst(){
+        if (size == 0){
+	    throw new NoSuchElementException();
+	}
+	T rem = data[first];
+	data[first] = null;
+	if(first == data.length - 1){
+	    first = 0;
+	}
+	else{first++;}
+	size--;
+	return rem;
+    }
+
+    public T removeLast(){
+	if (size == 0){
+	  throw new NoSuchElementException();
+	}
+	T rem = data[last];
+	data[last] = null;
+	if(last == 0){
+	    last = data.length - 1;
+	}
+	else{last--;}
+	size--;
+	return rem;
+    }
+
+    public T getFirst(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
 	return data[first];
     }
 
-    public E removeLast(){
+    public T getLast(){
+	if (size == 0){
+	    throw new NoSuchElementException();
+	}
 	return data[last];
     }
 
-    public E getFirst(){
-	return data[first];
+     @SuppressWarnings("unchecked")
+     public void resize(){
+	 T[]temp =  (T[]) new Object[size * 2];
+	 if(first > last){
+	     int index = 0;
+	     for(int i = first; i < size; i++){
+		 temp[index]= data[i];
+		 index++;
+	     }
+	     for(int i = 0 ; i <= last; i++){
+		 temp[index] = data[i];
+		 index++;
+	     }
+	 }
+	 else{for(int i = 0;i < size; i++){
+		 temp[i] = data[i];
+	     }
+	 }
+	 data = temp;
+	 first = 0;
+	 last = size - 1;
+     }
+
+    public String toString(){
+	String s = "";
+	for(int i = 0; i< data.length; i++){
+	    s += data[i];
+	}
+	return s;
     }
 
-    public E getLast(){
-	return data[last];
-    }
-    
+ 
 }
