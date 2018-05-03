d22ccb0ba55ddad32ea9a44923d00572123288c3
unknown <gzhou2@stuy.edu>
2018-05-03 18:19:04 -0400

    need pushUp and pushDown

--- 12HeapsofWork/MyHeap.java
+++ 12HeapsofWork/MyHeap.java
@@ -5,23 +5,67 @@ public class MyHeap<T extends Comparable<T>>{
     @SuppressWarnings("unchecked")
     public MyHeap(){
 	max = true;
-	length = 0;
+        size = 0;
 	data = (T[])new Comparable[10];
     }
     @SuppressWarnings("unchecked")
     public MyHeap(boolean a){
 	max = a;
-	length = 0;
+	size = 0;
 	data = (T[])new Comparable[10];
     }
 
+    public int size(){
+	return size;
+    }
+    
     public void add(T s){
-	if(length == 0){
-	    data[0] = s;
+	if(size == data.length){
+	    resize();
 	}
-	else if(){
-	}
+	data[size] = s;
+	size++;
+	pushUp();
+    }
+    
+    public T remove(){
+	T s = data[0];
+	swap(0, size()-1);
+	length--;	
+	pushDown();	
+	return s;
+    }
+    
+    public void swap (int a, int b){
+	T c = data[a];
+	data[a] = data[b];
+	data[b] = c;
     }
 
-    
+      @SuppressWarnings("unchecked")
+      private void resize(){
+	  T[] temp = (T[])new Comparable[2*size];
+	  for (int i = 0; i < size; i++){
+	      temp[i] = data[i];
+	  }
+	  data = temp;	
+      }
+
+    public String toString(){
+	String str = "[ ";
+	for(int i = 0; i <size;i++){
+	    line += data[i] + " , ";
+	}
+	return str + " ]";
+    }
+
+    public T peek(){
+	return data[0];
+    }
+
+    public void pushUp(){
+	
+    }
+    public void pushDown(){
+    }
 		     
