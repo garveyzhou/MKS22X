e1b8c863d12570571f1eb557c384ceae5a49b5b7
GarveyZhou <gzhou2@stuy.edu>
2018-05-16 15:29:40 -0400

    added extra test cases for maze

--- 04aMazed/Driver.java
+++ 04aMazed/Driver.java
@@ -7,7 +7,7 @@ public class Driver{
 	    Maze f = new Maze("data.txt");
 	    System.out.println(f.solve());
 	}
-	catch(FileNotFoundException e){}
+	catch(FileNotFoundException e){System.out.println("Error");}
 	try{
 	    Maze a = new Maze("maze.txt");
 	    a.setAnimate(true);
@@ -15,8 +15,12 @@ public class Driver{
 	    Maze b = new Maze("maze1.txt");
 	    b.setAnimate(true);
 	    System.out.println( b.solve());
+	    Maze c = new Maze("maze3.txt");
+	    c.setAnimate(true);
+	    System.out.println( c.solve());
 	}
 	catch (FileNotFoundException e){
+	    System.out.println("Error");
 	}
     }
 }
