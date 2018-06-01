25e0d8da242e0c8ce87caf87dd6b812e5347cbfe
unknown <gzhou2@stuy.edu>
2018-05-31 22:45:13 -0400

    changed heap for running medians

--- 13Meds/MyHeap.java
+++ 13Meds/MyHeap.java
@@ -24,8 +24,8 @@ public class MyHeap<T extends Comparable<T>>{
 	    resize();
 	}
 	data[size] = s;
-	size++;
 	swapUp(size - 1);
+	size++;
     }
     
     public T remove(){
@@ -37,14 +37,14 @@ public class MyHeap<T extends Comparable<T>>{
     }
 
 
-      @SuppressWarnings("unchecked")
-      private void resize(){
-	  T[] temp = (T[])new Comparable[2*size];
-	  for (int i = 0; i < size; i++){
-	      temp[i] = data[i];
-	  }
-	  data = temp;	
-      }
+    @SuppressWarnings("unchecked")
+    private void resize(){
+	T[] temp = (T[])new Comparable[2*size];
+	for (int i = 0; i < size; i++){
+	    temp[i] = data[i];
+	}
+	data = temp;	
+    }
 
     public String toString(){
 	String str = "[ ";
@@ -66,46 +66,70 @@ public class MyHeap<T extends Comparable<T>>{
     
     public void swapUp(int index){
 	int parent = (index - 1)/2;
-	if(index != 0){
+	if(index != 0 && parent > 0){
 	    if(max && data[index].compareTo(data[parent]) > 0){
 		swap(index,parent);}
-	    else if(max && data[index].compareTo(data[parent]) > 0){
+	    else if(!max && data[index].compareTo(data[parent]) > 0){
 		swap(index,parent);}
 	    swapUp(parent);
 	}
     }
+
+    
     public void swapDown(int index){
 	int left = index*2+1;
 	int right = index*2+2;
-	if( right < size){
-	    if (max){
-		if(data[left].compareTo(data[right]) < 0){
-		    if(data[index].compareTo(data[left]) < 0){
-			swap(index, left);	    
-			swapDown(left);
-		    }
-		}
-		else{if(data[index].compareTo(data[right]) < 0){
-			swap(index, right);	    
-			swapDown(right);		
-		    }
-		}
+	if(max){
+	    if((left < size && data[index].compareTo(data[left]) < 0)&&(right >= size || data[right].compareTo(data[left]) <= 0)){
+		swap(index, left);	    
+		swapDown(left);
 	    }
-	    if(!max){
-		if(data[left].compareTo(data[right]) > 0){
-		    if(data[index].compareTo(data[left]) > 0){
-			swap(index, left);	    
-			swapDown(left);
-		    }
-		}
-		else{if(data[index].compareTo(data[right]) > 0){
-			swap(index, right);	    
-			swapDown(right);		
-		    }
-		}
+	    if((right < size && data[index].compareTo(data[right]) < 0) && (left >= size || data[left].compareTo(data[right]) <= 0)){
+		swap(index, right);	    
+		swapDown(right);
 	    }
 	}
+	else{
+	    if((left < size && data[index].compareTo(data[left]) > 0)&&(right >= size || data[right].compareTo(data[left]) >= 0)){
+		swap(index, left);	    
+		swapDown(left);
+	    }
+	    if((right < size && data[index].compareTo(data[right]) > 0)&&(left >= size || data[left].compareTo(data[right]) >= 0)){
+		swap(index, right);	    
+		swapDown(right);
+	    }
+	}   
     }
+
+    
+    private boolean compares(boolean maxmin, T child, T parent){
+	if(maxmin){
+	    return child.compareTo(parent) > 0;
+	}
+	else{
+	    return child.compareTo(parent) < 0;
+	}
+    }
+    
+    public static void main(String[] args){
+	MyHeap<String> heap = new MyHeap<>(false);
+	heap.add("f");
+	System.out.println(heap);
+	heap.add("d");
+	System.out.println(heap);
+	heap.add("c");
+	System.out.println(heap);
+	heap.add("b");
+	System.out.println(heap);
+	System.out.println(heap.remove());
+	System.out.println(heap);
+	System.out.println(heap.remove());
+	System.out.println(heap);
+	System.out.println(heap.remove());
+	System.out.println(heap);
+	System.out.println(heap.remove());
+	System.out.println(heap);
+  
+    }
+    
 }
-	    
-	
